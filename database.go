b7687ea80fde15a5b360d603b75a7e8b2c04cb16
寺川航平 <odayakalife@terakawakouheis-MacBook-Pro.local>
2020-10-29 23:20:32 +0900

    vote_POST実装migrate追記

--- database.go
+++ database.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-  
-    "os"
-   
+	"os"
 
-    "github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm"
 )
 
 var db *gorm.DB
 
 func init() {
-    var err error
-    var datasource string
-    
-    if os.Getenv("DATABASE_URL") != "" {
-        // Heroku用
-datasource = "bc70a65d138ae3:14ea8170@tcp(us-cdbr-east-02.cleardb.com:3306)/heroku_6a3970f299d4fe5?parseTime=true&charset=utf8&loc=Local"
-    } else {
-        // ローカル用
-        datasource = "bc70a65d138ae3:14ea8170@/localhost?parseTime=true&charset=utf8&loc=Local"
-    }
-    db, err = gorm.Open("mysql",datasource )
-    if err != nil {
-      
-        panic("failed to connect database")
-    }
+	var err error
+	var datasource string
 
-    db.AutoMigrate(&Product{})
-    db.AutoMigrate(&Comment{})
+	if os.Getenv("DATABASE_URL") != "" {
+		// Heroku用
+		datasource = "bc70a65d138ae3:14ea8170@tcp(us-cdbr-east-02.cleardb.com:3306)/heroku_6a3970f299d4fe5?parseTime=true&charset=utf8&loc=Local"
+	} else {
+		// ローカル用
+		datasource = "bc70a65d138ae3:14ea8170@/localhost?parseTime=true&charset=utf8&loc=Local"
+	}
+	db, err = gorm.Open("mysql", datasource)
+	if err != nil {
+
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Product{})
+	db.AutoMigrate(&Comment{})
+	db.AutoMigrate(&Work{})
 }
