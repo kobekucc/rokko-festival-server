0dc15fc72fcae6497a8fc54c4b7f4476212145c7
寺川航平 <odayakalife@terakawakouheis-MacBook-Pro.local>
2020-10-29 23:15:27 +0900

    Revert "voteのPUT,POST実装"
    This reverts commit fbac7f8e2d842a31867b37e57e2909f0fcbcc438.

--- main.go
+++ main.go
@@ -30,13 +30,8 @@ func main() {
 	e.GET("/products", indexProduct)
 	e.GET("/products/:id", showProduct)
 
-	e.GET("/votes/:type/:id", getVotesNum)
-
 	e.POST("/products", createProduct)
 	e.POST("/comment/:type/:id/:comment", createComment)
-	e.POST("/vote/:type/:id/", createVote) //初めて投票されるとき
-
-	e.PUT("/vote/:type/:id", incrementVote)
 
 	e.Logger.Fatal(e.Start(":" + port))
 }
@@ -64,23 +59,4 @@ func createComment(c echo.Context) error {
 	comment := Comment{Type: c.Param("type"), WorkId: uint(id), Comment: c.Param("comment")}
 	db.Create(&comment)
 	return c.JSON(http.StatusOK, comment)
-}
-func getVotesNum(c echo.Context) error {
-	var count int
-	db.Where("type = ? AND work_id = ?", c.Param("type"), c.Param("id")).Count(&count)
-	return c.JSON(http.StatusOK, count)
-}
-func incrementVote(c echo.Context) error {
-	var work Work
-	db.Where("type = ? AND work_id = ?", c.Param("type"), c.Param("id")).First(&work)
-
-	work.Vote++
-	db.Save(&work)
-	return c.JSON(http.StatusOK, work)
-}
-func createVote(c echo.Context) error {
-	id, _ := strconv.ParseUint(c.Param("id"), 10, 64)
-	work := Work{Type: c.Param("type"), WorkId: uint(id), Vote: 1}
-	db.Create(&work)
-	return c.JSON(http.StatusOK, work)
 }
