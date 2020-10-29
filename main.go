8604e0c29236e386047b63ed9916b20eaefb65a2
寺川航平 <odayakalife@terakawakouheis-MacBook-Pro.local>
2020-10-29 23:23:52 +0900

    vote_PUT,GET実装

--- main.go
+++ main.go
@@ -29,10 +29,13 @@ func main() {
 	// routing
 	e.GET("/products", indexProduct)
 	e.GET("/products/:id", showProduct)
+	e.GET("/votes/:type/:id", getVotesNum)
 
 	e.POST("/products", createProduct)
 	e.POST("/comment/:type/:id/:comment", createComment)
 	e.POST("/vote/:type/:id", createVote)
+
+	e.PUT("/vote/:type/:id", incrementVote)
 
 	e.Logger.Fatal(e.Start(":" + port))
 }
@@ -65,5 +68,18 @@ func createVote(c echo.Context) error {
 	id, _ := strconv.ParseUint(c.Param("id"), 10, 64)
 	work := Work{Type: c.Param("type"), WorkId: uint(id), Vote: 1}
 	db.Create(&work)
+	return c.JSON(http.StatusOK, work)
+}
+func getVotesNum(c echo.Context) error {
+	var count int
+	db.Where("type = ? AND work_id = ?", c.Param("type"), c.Param("id")).Count(&count)
+	return c.JSON(http.StatusOK, count)
+}
+func incrementVote(c echo.Context) error {
+	var work Work
+	db.Where("type = ? AND work_id = ?", c.Param("type"), c.Param("id")).First(&work)
+
+	work.Vote++
+	db.Save(&work)
 	return c.JSON(http.StatusOK, work)
 }
